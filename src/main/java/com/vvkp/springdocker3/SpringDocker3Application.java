a8e02e1f35fb6e0a9c57d77ff5cc4d1658c9cef1
vivekTrantor <vivek.pandey@trantorinc.com>
2023-11-23 11:06:07 +0530

    first commit

---
+++ src/main/java/com/vvkp/springdocker3/SpringDocker3Application.java
@@ -0,0 +1,13 @@
+package com.vvkp.springdocker3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocker3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDocker3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vvkp/springdocker3/SpringDocker3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvkp.springdocker3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDocker3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
